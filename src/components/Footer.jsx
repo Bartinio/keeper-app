1123514aa02377d7e654653c7f106b0d46f66381
Karen <karenchakhalyan23@gmail.com>
2021-02-27 00:34:04 +0400

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,9 @@ import React from "react";
 function Footer() {
     const currentYear = new Date().getFullYear();       
     return (
-        <>
             <footer>
                  <p> Copyright &copy; {currentYear}</p>
              </footer>
-        </>
     )
     
 }
