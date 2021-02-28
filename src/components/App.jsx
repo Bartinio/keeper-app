d0d086ddda22470918c82d16a134ca989e910a6f
Karen <karenchakhalyan23@gmail.com>
2021-02-28 20:32:06 +0400

    testing something

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
+function createEntry(props) {
+    return <Note 
+        key = {props.key}
+        title = {props.title}
+        content = {props.content}
+    />
+}
 
 function App(){
     return (
         <>
         <Header />
-        <Note />
+        {notes.map(createEntry)}
         <Footer />
         </>
     )
